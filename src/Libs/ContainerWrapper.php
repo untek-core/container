7601e17fcba274b612b671ca9824c85b8cdb83f3
vitaliy <zndev@ya.ru>
2023-03-31 10:30:06 +0600

    upd

---
+++ src/Libs/ContainerWrapper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Untek\Core\Container\Libs;
+
+use Psr\Container\ContainerInterface;
+
+class ContainerWrapper implements ContainerInterface {
+    
+    public function __construct(private ContainerInterface $container)
+    {
+    }
+
+    public function get(string $id)
+    {
+        return $this->container->get($id);
+    }
+
+    public function has(string $id): bool
+    {
+        return $this->container->has($id);
+    }
+}
